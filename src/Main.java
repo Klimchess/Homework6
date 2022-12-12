b10c4ed4472af40d2440dd430afb40d11a6e0655
klimchess <vanyusha.1996@mail.ru>
2022-12-12 12:34:21 +0300

    решил 7 задач

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,43 @@
 public class Main {
     public static void main(String[] args) {
         //задача 1
-        for (int i = 0; i < 11; i = i + 1 ) {
-            System.out.println(i);}
+        for (int i = 0; i < 11; i = i + 1) {
+            System.out.println(i);
+        }
 
         //задача 2
         for (int i = 10; i > 0; i = i - 1) {
-            System.out.println(i);}
+            System.out.println(i);
+        }
 
         //задача 3
         for (int i = 0; i < 17; i = i + 2) {
-            System.out.println(i);}
+            System.out.println(i);
+        }
 
         //задача 4
         for (int i = 10; i > -11; i = i - 1) {
-            System.out.println(i); };
+            System.out.println(i);
+        }
+
+        //задача 5
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");}
+
+        //задача 6
+        for (int i = 7; i <= 98; i = i + 7 )
+        {System.out.println(i);}
+
+        //задача 7
+        for (int i = 1; i <=512; i = i * 2)
+        {System.out.println(i);}
+
+
+
+
 
 
     }
 
-}
+    }
+
