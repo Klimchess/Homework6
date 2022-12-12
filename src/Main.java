e8e44bc0b3fa194c3bda186e3f902c56a67b6341
klimchess <vanyusha.1996@mail.ru>
2022-12-12 12:22:42 +0300

    решил 4 задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задача 1
+        for (int i = 0; i < 11; i = i + 1 ) {
+            System.out.println(i);}
+
+        //задача 2
+        for (int i = 10; i > 0; i = i - 1) {
+            System.out.println(i);}
+
+        //задача 3
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);}
+
+        //задача 4
+        for (int i = 10; i > -11; i = i - 1) {
+            System.out.println(i); };
+
+
     }
+
 }
